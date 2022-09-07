1b18e550d9d5806eeb7f25405304019254bbd3d1
KayodeOusi <kayodeolusi@gmail.com>
2022-09-07 18:24:01 +0100

    ...

--- arrays/231_cotains-duplicate/index.js
+++ arrays/231_cotains-duplicate/index.js
@@ -10,7 +10,6 @@ const containsDuplicate = (nums) => {
   return false;
 };
 
-// Language: javascript
 console.log(containsDuplicate([1, 2, 3, 1]));
 console.log(containsDuplicate([1, 2, 3, 4]));
 console.log(containsDuplicate([1, 1, 1, 3, 3, 4, 3, 2, 4, 2]));
